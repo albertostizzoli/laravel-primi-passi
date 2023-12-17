af8f4989729875de542a06690059abf3ea216e2d
albertostizzoli <albertostizzoli@yahoo.it>
2023-12-17 10:10:35 +0100

    modified Hello World

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title> Laravel Primi Passi</title>
-    </head>
-    <body>
-        <h1>Hello World!!!!!</h1>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title> Laravel Primi Passi</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-danger text-center">Hello World!!!!!</h1>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
